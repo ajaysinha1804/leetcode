6a0eacff0905fac0189f176096b267ea77118faf
ajaysinha1804 <125199089+ajaysinha1804@users.noreply.github.com>
2023-08-04 15:28:11 +0530

    Time: 68 ms (88.41%), Space: 17.7 MB (15.43%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+       prevmap={}
+       for i,n in enumerate(nums):
+           diff=target-n
+           if diff in prevmap:
+               return [prevmap[diff],i]
+           prevmap[n]=i
