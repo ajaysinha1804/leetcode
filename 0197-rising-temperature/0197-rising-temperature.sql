c729a5952a37fe8f90c3a15c695a7168dc86c9b5
ajaysinha1804 <125199089+ajaysinha1804@users.noreply.github.com>
2023-09-28 15:16:00 +0530

    Time: 986 ms (50.41%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,7 +1,11 @@
 WITH cte AS
 (SELECT *,DATE_ADD(recordDate,INTERVAL-1 DAY) AS yesterdays_day
-, LAG(recordDate) OVER(ORDER BY recordDate) AS prev_recordDate,LAG(temperature) OVER(ORDER BY recordDate) AS prev_temp
+, LAG(recordDate) OVER(ORDER BY recordDate) AS prev_recordDate
+ ,LAG(temperature) OVER(ORDER BY recordDate) AS prev_temp
 FROM Weather)
+
+
 SELECT id
 FROM cte
-WHERE prev_recordDate = yesterdays_day AND temperature > prev_temp
+WHERE prev_recordDate = yesterdays_day 
+AND temperature > prev_temp
