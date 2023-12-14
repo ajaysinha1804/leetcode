92260e8f6266294bddf81f54cf829f9c985b8ca4
ajaysinha1804 <125199089+ajaysinha1804@users.noreply.github.com>
2023-12-14 15:25:55 +0530

    Time: 1426 ms (37.86%), Space: 51.5 MB (64.29%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -2,20 +2,16 @@ from typing import List
 
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
-        m = len(grid)
-        n = len(grid[0])
-
-        ones_row = [0] * m
-        ones_col = [0] * n
-
-        for i in range(m):
-            for j in range(n):
-                ones_row[i] += grid[i][j]
-                ones_col[j] += grid[i][j]
-
-        diff = [[0] * n for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
-                diff[i][j] = 2 * ones_row[i] + 2 * ones_col[j] - n - m
-
+        row=len(grid)
+        col=len(grid[0])
+        onesRow=[0]*row
+        onesCol=[0]*col
+        for i in range(row):
+            for j in range(col):
+                onesRow[i] +=grid[i][j]
+                onesCol[j] +=grid[i][j]
+        diff=[[0]*col for _ in range(row)]
+        for i in range(row):
+            for j in range(col):
+                diff[i][j]=2*onesRow[i]+ 2*onesCol[j]-row-col 
         return diff
