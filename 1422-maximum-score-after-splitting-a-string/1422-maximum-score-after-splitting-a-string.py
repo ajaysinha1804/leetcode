bdcbee10b31a9ee1ead2b37633d382ba33435d29
ajaysinha1804 <125199089+ajaysinha1804@users.noreply.github.com>
2023-12-22 19:14:23 +0530

    Time: 111 ms (9.19%), Space: 17.1 MB (5.69%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
@@ -3,12 +3,16 @@ class Solution:
         ans=0
         for i in range(len(s)-1):
             curr=0
+            left_curr=0
+            right_curr=0
+            res=0
             for j in range(i+1):
                 if s[j]=="0":
-                    curr +=1
+                    left_curr +=1
             for j in range(i+1,len(s)):
                 if s[j]=="1":
-                    curr +=1
-            ans =max(ans,curr)
+                    right_curr +=1
+            res=left_curr + right_curr
+            ans =max(ans,res)
         return ans
                 
