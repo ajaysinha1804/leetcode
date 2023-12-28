2c54b3512df88e7e5c1a6f9703b458ea7c81789f
ajaysinha1804 <125199089+ajaysinha1804@users.noreply.github.com>
2023-12-28 16:24:51 +0530

    Time: 3357 ms (5.00%), Space: 17.6 MB (94.00%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.py
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -5,7 +5,7 @@ class Solution:
         dp[0][0] = 0
 
         for i in range(1, n + 1):
-            for j in range(0, k + 1):
+            for j in range(k + 1):
                 cnt, del_ = 0, 0
                 for l in range(i, 0, -1):
                     if s[l - 1] == s[i - 1]:
