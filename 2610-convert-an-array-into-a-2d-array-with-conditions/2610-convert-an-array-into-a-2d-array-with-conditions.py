4d50a39effb09031fc6277c0fcdbcd91d32db576
ajaysinha1804 <125199089+ajaysinha1804@users.noreply.github.com>
2024-01-02 23:41:47 +0530

    Time: 120 ms (5.01%), Space: 17.4 MB (5.17%) - LeetHub

---
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        freq = [0] * (len(nums) + 1)
+        
+        ans = []
+        for c in nums:
+            if freq[c] >= len(ans):
+                ans.append([])
+            
+            # Store the integer in the list corresponding to its current frequency.
+            ans[freq[c]].append(c)
+            freq[c] += 1
+        
+        return ans
