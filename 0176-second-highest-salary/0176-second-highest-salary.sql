8797c7af418a2a7dcd358594f2e12e73d8416b6c
ajaysinha1804 <125199089+ajaysinha1804@users.noreply.github.com>
2024-01-12 15:23:05 +0530

    Time: 967 ms (5.68%), Space: 0B (100.00%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT (
+SELECT DISTINCT salary AS SecondHighestSalary
+FROM Employee
+ORDER BY salary DESC 
+LIMIT 1 OFFSET 1
+    ) AS SecondHighestSalary
